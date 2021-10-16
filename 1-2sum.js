9b2ac6550f30a1bec9816c8d4c6833e9263aff07
bignuttawuts <48540841+bignuttawuts@users.noreply.github.com>
2021-10-16 13:28:41 +0700

    using hash map

--- 1-2sum.js
+++ 1-2sum.js
@@ -3,19 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    var result = []
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i+1; j < nums.length; j++) {
-            var numI = nums[i]
-            var numJ = nums[j]
-            if ((numI + numJ) === target) {
-                result = [i, j]
-            }
-        }
-        if (result.length === 2)
-            break;
+var twoSum = function (nums, target) {
+  var map = new Map();
+  for (var i = 0; i < nums.length; i++) {
+    var complement = target - nums[i];
+    if (map.has(complement)) {
+      return [i, map.get(complement)];
     }
-    
-    return result
+    map.set(nums[i], i);
+  }
 };
